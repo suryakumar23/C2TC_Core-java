afda403165e4265e4c2a81c8d57e33170290ddce
Dell <Dell@DESKTOP-NVGSRBK>
2022-06-03 21:27:55 +0530

    first commit

---
+++ project/src/project/Main.java
@@ -0,0 +1,15 @@
+package project;
+class Student {
+	int id;
+	String name;
+}
+public class Main {	
+	public static void main(String[] args) {
+                  Student s1 = new Student();
+                  s1.id = 108;
+                  s1.name = "surya";
+                  System.out.println(s1.id);
+                  System.out.println(s1.name);
+	}
+
+}
